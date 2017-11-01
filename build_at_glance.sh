038ab7d4c4fabadd02e8516b477b503aaaf79a67
Filip Ginter <ginter@cs.utu.fi>
2017-11-01 14:53:40 +0200

    script to build at glance

---
+++ build_at_glance.sh
@@ -0,0 +1,2 @@
+python3 at_glance.py --codes codes_and_flags.yaml --genre genre_symbols.json --skip empty _corpus_metadata/*.json > docs-src/_includes/at_glance.html
+python3 at_glance.py --codes codes_and_flags.yaml --genre genre_symbols.json --skip with_data _corpus_metadata/*.json > docs-src/_includes/at_glance_empty.html
