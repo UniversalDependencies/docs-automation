aaffd3718a2afc82a78b2887bd888c891d15f6c7
Filip Ginter <ginter@cs.utu.fi>
2017-11-20 14:47:42 +0200

    make docs-dir explicit in the scripts

--- build_at_glance.sh
+++ build_at_glance.sh
@@ -1,2 +1,2 @@
-python3 at_glance.py --codes codes_and_flags.yaml --genre genre_symbols.json --skip empty _corpus_metadata/*.json > docs-src/_includes/at_glance.html
-python3 at_glance.py --codes codes_and_flags.yaml --genre genre_symbols.json --skip withdata _corpus_metadata/*.json > docs-src/_includes/at_glance_empty.html
+python3 at_glance.py --codes codes_and_flags.yaml --genre genre_symbols.json --skip empty --docs-dir docs-src _corpus_metadata/*.json > docs-src/_includes/at_glance.html
+python3 at_glance.py --codes codes_and_flags.yaml --genre genre_symbols.json --skip withdata --docs-dir docs-src _corpus_metadata/*.json > docs-src/_includes/at_glance_empty.html
