239b1e27b651b6191856264192e54ddabb0fd439
Dan Zeman <zeman@ufal.mff.cuni.cz>
2018-01-31 12:26:57 +0100

    If www-data clones a repository, access for zeman must be granted.

--- valdan/clone_one.sh
+++ valdan/clone_one.sh
@@ -7,9 +7,11 @@ git clone https://github.com/UniversalDependencies/$i.git
 cd $i
 git checkout dev
 cd ..
-# We must grant access permissions for the user www-data.
+# We must grant access permissions for the users zeman and www-data.
+# If either of them creates the folder, the other must be able to add and remove files.
 # We cannot do it using chmod because it would mean a local modification and git would refuse to pull.
 # However, we can use access control lists (acl).
 setfacl -R -m u:www-data:rwx $i
+setfacl -R -m u:zeman:rwx $i
 setfacl -R -d -m u:www-data:rwx $i
 setfacl -R -d -m u:zeman:rwx $i
