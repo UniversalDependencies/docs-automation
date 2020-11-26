2b14d4483b2f1055df253bc41bcfd7a896273579
Dan Zeman <zeman@ufal.mff.cuni.cz>
2020-11-26 17:08:01 +0100

    Save the documented features in JSON.

--- valrules/scan_docs_for_feats.pl
+++ valrules/scan_docs_for_feats.pl
@@ -8,6 +8,7 @@ use open ':utf8';
 binmode(STDIN, ':utf8');
 binmode(STDOUT, ':utf8');
 binmode(STDERR, ':utf8');
+use YAML qw(LoadFile);
 
 # At present, the path to the local copy of docs is hardwired.
 my $docs = 'C:/Users/Dan/Documents/Lingvistika/Projekty/universal-dependencies/docs';
@@ -75,7 +76,8 @@ foreach my $langfolder (@langfolders)
     }
 }
 # Print an overview of the features we found.
-print_markdown_overview(\%hash, \%lhash);
+#print_markdown_overview(\%hash, \%lhash);
+print_json(\%hash, \%lhash, $docs);
 
 
 
@@ -232,4 +234,167 @@ sub print_markdown_overview
         }
         print("\n");
     }
+}
+
+
+
+#------------------------------------------------------------------------------
+# Prints a JSON structure with documented feature-value pairs for each UD
+# language.
+#------------------------------------------------------------------------------
+sub print_json
+{
+    my $ghash = shift; # ref to hash with global features
+    my $lhash = shift; # ref to hash with local features
+    # We need to know the list of all UD languages first.
+    my $docspath = shift;
+    my $languagespath = "$docspath/../docs-automation/codes_and_flags.yaml";
+    my $languages = LoadFile($languagespath);
+    if( !defined($languages) )
+    {
+        die "Cannot read the list of languages";
+    }
+    my @lcodes = sort(map {$languages->{$_}{lcode}} (keys(%{$languages})));
+    print("{\n");
+    foreach my $lcode (@lcodes)
+    {
+        my @fvpairs = ();
+        # Add locally defined (or redefined) features.
+        if(exists($lhash->{$lcode}))
+        {
+            foreach my $feature (sort(keys(%{$lhash->{$lcode}})))
+            {
+                # Skip the feature if there are errors in its documentation.
+                unless(scalar(@{$lhash->{$lcode}{$feature}{errors}}) > 0)
+                {
+                    foreach my $value (sort(@{$lhash->{$lcode}{$feature}{values}}))
+                    {
+                        push(@fvpairs, "$feature=$value");
+                    }
+                }
+            }
+        }
+        # Add globally defined features that are not redefined locally.
+        foreach my $feature (sort(keys(%{$ghash})))
+        {
+            unless(exists($lhash->{$lcode}{$feature}))
+            {
+                # Skip the feature if there are errors in its documentation.
+                unless(scalar(@{$ghash->{$feature}{errors}}) > 0)
+                {
+                    foreach my $value (sort(@{$ghash->{$feature}{values}}))
+                    {
+                        push(@fvpairs, "$feature=$value");
+                    }
+                }
+            }
+        }
+        print('"'.escape_json_string($lcode).'": ['.join(', ', map {'"'.escape_json_string($_).'"'} (@fvpairs)).'],'."\n");
+    }
+    print("}\n");
+}
+
+
+
+#------------------------------------------------------------------------------
+# Takes a list of pairs [name, value] and returns the corresponding JSON
+# structure {"name1": "value1", "name2": "value2"}. The pair is an arrayref;
+# if there is a third element in the array and it says "numeric", then the
+# value is treated as numeric, i.e., it is not enclosed in quotation marks.
+# The type in the third position can be also "list" (of strings),
+# "list of numeric" and "list of structures".
+#------------------------------------------------------------------------------
+sub encode_json
+{
+    my @json = @_;
+    # Encode JSON.
+    my @json1 = ();
+    foreach my $pair (@json)
+    {
+        my $name = '"'.$pair->[0].'"';
+        my $value;
+        if(defined($pair->[2]))
+        {
+            if($pair->[2] eq 'numeric')
+            {
+                $value = $pair->[1];
+            }
+            elsif($pair->[2] eq 'list')
+            {
+                # Assume that each list element is a string.
+                my @array_json = ();
+                foreach my $element (@{$pair->[1]})
+                {
+                    my $element_json = $element;
+                    $element_json = escape_json_string($element_json);
+                    $element_json = '"'.$element_json.'"';
+                    push(@array_json, $element_json);
+                }
+                $value = '['.join(', ', @array_json).']';
+            }
+            elsif($pair->[2] eq 'list of numeric')
+            {
+                # Assume that each list element is numeric.
+                my @array_json = ();
+                foreach my $element (@{$pair->[1]})
+                {
+                    push(@array_json, $element);
+                }
+                $value = '['.join(', ', @array_json).']';
+            }
+            elsif($pair->[2] eq 'list of structures')
+            {
+                # Assume that each list element is a structure.
+                my @array_json = ();
+                foreach my $element (@{$pair->[1]})
+                {
+                    my $element_json = encode_json(@{$element});
+                    push(@array_json, $element_json);
+                }
+                $value = '['.join(', ', @array_json).']';
+            }
+            else
+            {
+                log_fatal("Unknown value type '$pair->[2]'.");
+            }
+        }
+        else # value is a string
+        {
+            if(!defined($pair->[1]))
+            {
+                die("Unknown value of attribute '$name'");
+            }
+            $value = $pair->[1];
+            $value = escape_json_string($value);
+            $value = '"'.$value.'"';
+        }
+        push(@json1, "$name: $value");
+    }
+    my $json = '{'.join(', ', @json1).'}';
+    return $json;
+}
+
+
+
+#------------------------------------------------------------------------------
+# Takes a string and escapes characters that would prevent it from being used
+# in JSON. (For control characters, it throws a fatal exception instead of
+# escaping them because they should not occur in anything we export in this
+# block.)
+#------------------------------------------------------------------------------
+sub escape_json_string
+{
+    my $string = shift;
+    # https://www.ietf.org/rfc/rfc4627.txt
+    # The only characters that must be escaped in JSON are the following:
+    # \ " and control codes (anything less than U+0020)
+    # Escapes can be written as \uXXXX where XXXX is UTF-16 code.
+    # There are a few shortcuts, too: \\ \"
+    $string =~ s/\\/\\\\/g; # escape \
+    $string =~ s/"/\\"/g; # escape " # "
+    if($string =~ m/[\x{00}-\x{1F}]/)
+    {
+        log_fatal("The string must not contain control characters.");
+    }
+    return $string;
 }
