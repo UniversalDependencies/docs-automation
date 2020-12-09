a0fa41b925bfab93783221f0b061783500a2279c
Dan Zeman <zeman@ufal.mff.cuni.cz>
2020-12-09 12:34:45 +0100

    Added deviations from Deixis.

--- valrules/scan_docs_for_feats.pl
+++ valrules/scan_docs_for_feats.pl
@@ -52,6 +52,8 @@ my @deviations =
      'msg' => "The correct UD feature for possessor's person is 'Person[psor]'."},
     {'re'  => 'PossNumber=.*',
      'msg' => "The correct UD feature for possessor's number is 'Number[psor]'."},
+    {'re'  => '(Dem|Distance)=.*',
+     'msg' => "Did you mean 'Deixis=Prox|Med|Remt|...'?"},
     {'re'  => 'NumForm=Letter',
      'msg' => "Did you mean 'NumForm=Word'?"},
     {'re'  => '(Gender|Animacy|NounClass)(\[[a-z]+\])?=Nonhum',
