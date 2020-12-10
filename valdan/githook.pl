5c94a5cebb07b7e2802e965b275ef5b6752f3508
Dan Zeman <zeman@ufal.mff.cuni.cz>
2020-12-10 21:49:06 +0100

    Hook on tools should also react to feats.json.

--- valdan/githook.pl
+++ valdan/githook.pl
@@ -111,7 +111,7 @@ if(defined($result))
                     my $ltcode = $2;
                     $changed{$ltcode}++;
                 }
-                elsif($file eq 'data/data.json' && $commit->{message} =~ m/Updated data specific for ([a-z]{2,3})\./)
+                elsif($file =~ m/^data\/(data|feats)\.json$/ && $commit->{message} =~ m/Updated data specific for ([a-z]{2,3})\./)
                 {
                     my $ltcode = $1;
                     $changed{$ltcode}++;
