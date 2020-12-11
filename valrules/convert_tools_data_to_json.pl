f1fb34c9cc9a714e7c5e640b8e65c4eef24eb90b
Dan Zeman <zeman@ufal.mff.cuni.cz>
2020-12-11 22:25:36 +0100

    Copied language-specific deprels from tools/data.

--- valrules/convert_tools_data_to_json.pl
+++ valrules/convert_tools_data_to_json.pl
@@ -9,6 +9,7 @@ binmode(STDIN, ':utf8');
 binmode(STDOUT, ':utf8');
 binmode(STDERR, ':utf8');
 use YAML qw(LoadFile);
+require valdata;
 
 my $ud = 'C:/Users/Dan/Documents/Lingvistika/Projekty/universal-dependencies';
 my $tools = "$ud/tools";
@@ -23,7 +24,7 @@ if ( !defined($languages) )
 # At the same time, separate family from genus where applicable.
 my %lname_by_code; map {$lname_by_code{$languages->{$_}{lcode}} = $_} (keys(%{$languages}));
 opendir(DIR, "$tools/data") or die("Cannot read folder '$tools/data': $!");
-my @files = grep {m/^feat_val\.[a-z]+$/ && -f "$tools/data/$_"} (readdir(DIR));
+my @files = grep {m/^deprel\.[a-z]+$/ && -f "$tools/data/$_"} (readdir(DIR));
 closedir(DIR);
 print STDERR ("Found ".scalar(@files)." files: ".join(', ', @files)."\n");
 my %data;
@@ -44,60 +45,18 @@ foreach my $file (@files)
     {
         chomp();
         s/\#.*//;
-        # Filter out feature-value pairs that have a wrong form.
-        if(!m/^[A-Z][A-Za-z0-9]*(\[[a-z]+\])?=[A-Z0-9][A-Za-z0-9]*$/)
+        # Filter out dependency relations that have a wrong form.
+        if(!m/^[a-z]+(:[a-z]+)?$/)
         {
             next;
         }
-        $data{$lcode}{$_}++;
+        $data{$lcode}{$_} = {'type' => 'lspec', 'permitted' => 1};
         $n++;
     }
     close(FILE);
     if($n==0)
     {
-        print STDERR ("WARNING: No valid feature values found in language '$lcode'\n");
+        print STDERR ("WARNING: No valid dependency relations found in language '$lcode'\n");
     }
 }
-# Write JSON.
-print("{\n");
-my @lcodes = sort(keys(%data));
-my @llines = ();
-foreach my $lcode (@lcodes)
-{
-    if(!exists($lname_by_code{$lcode}))
-    {
-        print STDERR ("WARNING: Skipping unknown language code '$lcode'\n");
-        next;
-    }
-    my $lline = '"'.escape_json_string($lcode).'": [';
-    $lline .= join(', ', map {'"'.escape_json_string($_).'"'} (sort(keys(%{$data{$lcode}}))));
-    $lline .= ']';
-    push(@llines, $lline);
-}
-print(join(",\n", @llines)."\n");
-print("}\n");
-
-
-
-#------------------------------------------------------------------------------
-# Takes a string and escapes characters that would prevent it from being used
-# in JSON. (For control characters, it throws a fatal exception instead of
-# escaping them because they should not occur in anything we export in this
-# block.)
-#------------------------------------------------------------------------------
-sub escape_json_string
-{
-    my $string = shift;
-    # https://www.ietf.org/rfc/rfc4627.txt
-    # The only characters that must be escaped in JSON are the following:
-    # \ " and control codes (anything less than U+0020)
-    # Escapes can be written as \uXXXX where XXXX is UTF-16 code.
-    # There are a few shortcuts, too: \\ \"
-    $string =~ s/\\/\\\\/g; # escape \
-    $string =~ s/"/\\"/g; # escape " # "
-    if($string =~ m/[\x{00}-\x{1F}]/)
-    {
-        log_fatal("The string must not contain control characters.");
-    }
-    return $string;
-}
+valdata::write_deprels_json(\%data, 'deprels.json');
