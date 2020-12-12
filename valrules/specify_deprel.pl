73329d090701a6d0fc5e8eb64a889ef9707fc49a
Dan Zeman <zeman@ufal.mff.cuni.cz>
2020-12-12 21:08:26 +0100

    Debugging.

--- valrules/specify_deprel.pl
+++ valrules/specify_deprel.pl
@@ -192,6 +192,7 @@ sub print_deprels_for_language
         {
             print("  <p><b>Undocumented dependency relations cannot be used:</b> ".join(', ', @undocumented)."</p>\n");
         }
+        print("  <p><b>DEBUGGING: All deprels known in relation to this language:</b> ".join(', ', @deprels)."</p>\n");
         my @errors = ();
         foreach my $f (@deprels)
         {
