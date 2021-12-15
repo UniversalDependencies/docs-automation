3fc18a603b47922fc73d29ad918a17ad6ece51bd
Dan Zeman <zeman@ufal.mff.cuni.cz>
2021-12-15 10:11:04 +0100

    Hint.

--- valrules/specify_edeprel.pl
+++ valrules/specify_edeprel.pl
@@ -641,6 +641,7 @@ EOF
 #------------------------------------------------------------------------------
 sub summarize_guidelines
 {
+    # cat UD_English-{PUD,EWT,GUM}/*.conllu | udapy -T util.Filter mark=1 keep_tree_if_node='len(node.deps)>=1 and "advcl:if" in [x["deprel"] for x in node.deps]' | less -R
     print <<EOF
   <h1><img class=\"flag\" src=\"https://universaldependencies.org/flags/png/$languages->{$lname_by_code{$config{lcode}}}{flag}.png\" />
     Specify enhanced dependency relations for $lname_by_code{$config{lcode}}</h1>
@@ -654,6 +655,9 @@ sub summarize_guidelines
     <tt><a href="https://universaldependencies.org/u/feat/Case.html">Case</a></tt>
     feature), or a combination of both. The official UD validator will accept
     case markers that are documented here.</p>
+  <p><a href="https://udapi.github.io/">Udapi</a> can be used to find examples
+    of enhanced dependency relation like this:
+    <pre>cat UD_English-{PUD,EWT,GUM}/*.conllu | udapy -T util.Filter mark=1 keep_tree_if_node='len(node.deps)&gt;=1 and "advcl:if" in [x["deprel"] for x in node.deps]' | less -R</pre></p>
 EOF
     ;
 }
