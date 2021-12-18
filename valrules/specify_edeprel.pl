8907861440ec0eba4e2d5111e267e346ab075c8f
Dan Zeman <zeman@ufal.mff.cuni.cz>
2021-12-18 11:06:06 +0100

    Difference (Czech o:acc).

--- valrules/specify_edeprel.pl
+++ valrules/specify_edeprel.pl
@@ -105,6 +105,7 @@ my @functions =
      [1, 'Similar to something (“like”, semblative)',                           'Sem'],
      [1, 'Dissimilar to something (“unlike, as opposed to”, dissemblative)',    'Dis'],
      [1, 'Better/worse/other than something (“than”, comparative)',             'Cmp'],
+     [1, 'Difference (“by how much”)'                                           'Dif'],
      [1, 'Comment (“whereas”)',                                                 'Cmt'], # I would argue that 'whereas' should be CCONJ but it is tagged SCONJ in the English corpora.
     [0, 'Cause/consequence/circumstance', undef], # (we want to display this comment but not to make it selectable)
      [1, 'Cause or purpose (“because of, due to, given, in order to, per”, causative)', 'Cau'],
