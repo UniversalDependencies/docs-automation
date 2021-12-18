5b89f3e77efb74e7b1de1ecc7dde1a3f3f59d764
Dan Zeman <zeman@ufal.mff.cuni.cz>
2021-12-18 12:19:09 +0100

    Frl.

--- valrules/specify_edeprel.pl
+++ valrules/specify_edeprel.pl
@@ -84,6 +84,7 @@ my @functions =
       [2, 'Target inside something (“into”, illative)',                         'Ill'],
       [2, 'Target atop or above something (“onto”, superlative)',               'Spl'],
       [2, 'Target under or below something (“to under”, sublative)',            'Sbl'],
+      [2, 'In front of something (“in front of, before”)',                      'Frl'],
     [0, 'Time (temporal)',                                                      'Tem'],
      [1, 'Before a point (“before, prior to, till, until”, antessive)',         'Ant'],
      [1, 'Around a point (“around, circa”)',                                    'Trd'],
