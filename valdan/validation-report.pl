ff3cb0d2791d8c37f754f82c9b7d08ac6dd33b5c
Dan Zeman <zeman@ufal.mff.cuni.cz>
2023-04-03 17:36:27 +0200

    colors

--- valdan/validation-report.pl
+++ valdan/validation-report.pl
@@ -93,7 +93,7 @@ while(<REPORT>)
     }
     elsif(m/(SAPLING|RETIRED) VALID/)
     {
-        $color = 'LightGreen';
+        $color = 'LightGreen;background:LightGray';
         $nvalid++;
         $languages_valid{$language}++;
     }
