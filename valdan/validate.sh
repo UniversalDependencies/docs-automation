9b6f2bf8d09cd2dd6dd28b73b5905759ce1e699e
Dan Zeman <zeman@ufal.mff.cuni.cz>
2025-06-19 16:49:38 +0200

    Updated environment before calling the validator.

--- valdan/validate.sh
+++ valdan/validate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Runs validate.py (format validation of UD treebanks).
-# Copyright © 2018 Dan Zeman <zeman@ufal.mff.cuni.cz>
+# Copyright © 2018, 2025 Dan Zeman <zeman@ufal.mff.cuni.cz>
 # License: GNU GPL
 
 # This script may be invoked from a CGI script. Therefore it must be able to
@@ -11,13 +11,17 @@
 export LC_ALL=en_US.utf8
 
 # Validate.py depends the regex module that may not be installed system-wide.
-# If we installed it locally using pip install --user regex, it is now in our
-# home: ~/.local/lib/python2.7/site-packages. However, user www-data will not
-# find the packages there. Hence we assume that ~/.local/lib/python2.7 has been
-# recursively copied to the pythonlib subfolder of the current folder.
-export PYTHONPATH=pythonlib/python3.4/site-packages:/usr/lib/cgi-bin/unidep/udapi-python
+# In addition, in June 2025 I am experimenting with making it dependent on
+# Udapi, which itself is dependent on colorama and termcolor. The on-line
+# validation will be invoked by user www-data, who must have access to the same
+# version of Python with the same packages installed. Therefore, I created a
+# virtual environment, installed the packages there, and I am activating the
+# environment here before we launch the validator. Moreover, I am not using the
+# version of Udapi from pip but I have a copy of udapi-python from GitHub
+# instead. The PYTHONPATH variable will tell Python where to find Udapi.
+source .venv/bin/activate
+export PYTHONPATH=/usr/lib/cgi-bin/unidep/udapi-python
 
 # Finally, we assume that validate.py itself resides in a local clone of the
-# tools repository from UD Github.
+# tools repository from UD GitHub.
 python3 tools/validate.py $*
-
