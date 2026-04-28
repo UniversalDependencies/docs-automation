9bc34bc0fef47af1728653094a8ec98011e17456
Dan Zeman <zeman@ufal.mff.cuni.cz>
2026-04-28 13:40:39 +0200

    More steps towards identifying oldest unused UD repos.

--- ghapi/gl.sh
+++ ghapi/gl.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Call this with --no-expand-tabs if we want to read and parse the output.
 
-git log --reverse --pretty='format:%C(auto)%ai%x09%h%x09%s'
+folder=$(pwd | perl -pe 'chomp; s:.*/::')
+conllu=$(ls -1 | grep -P '\.conllu' | wc -l)
+git log --reverse --pretty='format:'$folder'%x09'$conllu'%x09%C(auto)%ai%x09%h%x09%an%x09%s' $@
+
+# History of UD repo creation:
+# for i in UD_* ; do cd $i ; gl --no-expand-tabs | head -1 ; cd .. ; done | tee udrepoage.txt
+# grep -P '\t0\t' udrepoage.txt | sort -k3 -r
 
